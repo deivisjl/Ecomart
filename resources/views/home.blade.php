a373f1fbd77cac59e4e9332055687a32cb1974f7
deivisjl <deivisjl@gmail.com>
2019-05-24 10:02:27 -0600

    Set content view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,37 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-header text-center"><h4>Administración</h4></div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <a href="/categorias" class="btn btn-outline-info btn-block"><h5>Categorías</h5></a>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <a href="/productos" class="btn btn-outline-info btn-block"><h5>Productos</h5></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <a href="/pedidos" class="btn btn-outline-info btn-block"><h5>Pedidos</h5></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
